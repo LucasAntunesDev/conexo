4fe19368536ae3cc6646ed6b3476e25c59398819
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2024-03-02 11:50:27 -0300

    add: módulo de categoria (incompleto)

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Categoria;
+use App\Http\Controllers\Controller;
+
+class CategoriaController extends Controller {
+
+    public function index() {
+        $categorias = Categoria::all();
+
+        return view('categorias', [
+            'categorias' => $categorias
+        ]);
+    }
+
+    public function create() {
+        $categoria = new Categoria();
+
+        return view('Categoria', [
+            'Categoria' => $categoria
+        ]);
+    }
+
+    public function edit($id) {
+        $categoria = Categoria::find($id);
+
+        return view('Categoria', [
+            'Categoria' => $categoria
+        ]);
+    }
+
+    public function store(Request $request) {
+
+        $messages = [
+            'nome.required' => 'O campo título deve ser preenchido',
+            'disciplina_id.required' => 'O id da disciplina deve ser preenchido'
+        ];
+
+        $validator = Validator::make($request->all(), [
+            'nome' => 'required'
+        ], $messages);
+
+        if ($validator->fails()) return redirect()->route('categorianovo')->withErrors($validator)->withInput();
+        else {
+            $categoria = new Categoria();
+            $categoria->nome = $request->input('nome');
+            $categoria->save();
+
+            return redirect()->route('categorias');
+        }
+    }
+
+    public function update($id, Request $request) {
+        $validator = Validator::make($request->all(), [
+            'nome' => 'required'
+        ]);
+
+        if ($validator->fails()) return redirect()->route('categoriasnovo')->withErrors($validator)->withInput();
+        else {
+            $categoria = Categoria::find($id);
+            $categoria->nome = $request->input('nome');
+            $categoria->save();
+
+            return redirect()->route('categorias');
+        }
+    }
+
+    public function destroy($id) {
+        $categoria = Categoria::find($id);
+        $categoria->delete();
+
+        return redirect()->route('categorias');
+    }
+}
