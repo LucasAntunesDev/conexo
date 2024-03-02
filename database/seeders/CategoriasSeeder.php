3125a3c2bdbc28c6e2722f4245fc1fa0603479d8
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2024-03-02 08:42:13 -0300

    add: seeder de categorias

---
+++ database/seeders/CategoriasSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class CategoriasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categorias')->insert([
+            ['nome' => 'Advérbios de Lugar', 'disciplina_id' =>  1],
+            ['nome' => 'Fontes de Energia', 'disciplina_id' =>  1],
+            ['nome' => 'Participantes num processo judicial', 'disciplina_id' =>  1],
+            ['nome' => 'Notas Musicais', 'disciplina_id' =>  1],
+            ['nome' => 'Times de Futebol', 'disciplina_id' =>  1],
+            ['nome' => 'Bancos', 'disciplina_id' =>  1],
+            ['nome' => 'Estabelecimentos educacionais', 'disciplina_id' =>  1],
+            ['nome' => 'Cargos no mercado financeiro', 'disciplina_id' =>  1],
+            ['nome' => 'Comida', 'disciplina_id' =>  1],
+            ['nome' => 'Bairros do Rio de Janeiro', 'disciplina_id' =>  1],
+        ]);
+    }
+}
