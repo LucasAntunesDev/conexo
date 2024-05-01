b9964759ddedcbc4569fbf0f8d0f8c8fba51a6ce
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2024-05-01 08:42:19 -0300

    update: view de jogo

--- resources/views/conexo.blade.php
+++ resources/views/conexo.blade.php
@@ -1,65 +1,68 @@
 @extends('layout')
 @section('content')
 
-<h1 class="text-violet-500 text-5xl font-bold inline-flex gap-palavrasDescobertas-2 mt-auto">
-    <svg width="50" height="50" viewBox="0 0 192 192" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-            d="M2.25 15.9029C2.25 8.36262 8.36262 2.25 15.9029 2.25H176.097C183.637 2.25 189.75 8.36262 189.75 15.9029V37.2925C189.75 39.8059 187.712 41.8434 185.199 41.8434H6.80097C4.28754 41.8434 2.25 39.8059 2.25 37.2925V15.9029Z"
-            fill="#C2410C"></path>
-        <path
-            d="M2.25 55.9515C2.25 53.438 4.28754 51.4005 6.80097 51.4005H185.199C187.712 51.4005 189.75 53.438 189.75 55.9515V86.443C189.75 88.9564 187.712 90.9939 185.199 90.9939H6.80097C4.28754 90.9939 2.25 88.9564 2.25 86.443V55.9515Z"
-            fill="#047857"></path>
-        <path
-            d="M2.25 105.557C2.25 103.044 4.28754 101.006 6.80097 101.006H185.199C187.712 101.006 189.75 103.044 189.75 105.557V136.049C189.75 138.562 187.712 140.6 185.199 140.6H6.80097C4.28754 140.6 2.25 138.562 2.25 136.049V105.557Z"
-            fill="#0E7490"></path>
-        <path
-            d="M2.25 154.708C2.25 152.194 4.28754 150.157 6.80097 150.157H185.199C187.712 150.157 189.75 152.194 189.75 154.708V176.097C189.75 183.637 183.637 189.75 176.097 189.75H15.9029C8.36262 189.75 2.25 183.637 2.25 176.097V154.708Z"
-            fill="#6D28D9"></path>
-    </svg>
-    CONEXO
-</h1>
+<div class="w-screen">
 
-<div id="acertou" class="hidden">
-    <span class="font-bold mx-auto inline-flex">Parabéns!</span>
-    <p>
-        Você conseguiu em <span id="acertouNumeroTentativas"></span> tentativas.
-    </p>
+    <div id="acertou" class="hidden">
+        <span class="font-bold mx-auto inline-flex">Parabéns!</span>
+        <p>
+            Você conseguiu em <span id="acertouNumeroTentativas"></span> tentativas.
+        </p>
 
-    <span class="bg-violet-500 font-semibold rounded-lg p-3 w-fit inline-flex gap-palavrasDescobertas-2
+        <span class="bg-violet-500 font-semibold rounded-lg p-3 w-fit inline-flex gap-x-2
             hover:bg-violet-600 hover:cursor-pointer mx-auto">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="stroke-zinc-50 size-6">
-            <path stroke-linecap="round" stroke-linejoin="round"
-                d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
-        </svg>
-        Compartilhe
-    </span>
-</div>
-
-<div class="mx-auto">
-    <strong id="teste">Palavras: </strong>
-    <br></br>
-    <span class="font-semibold" id="dataAtual"></span>
-    <span class="capitalize">acertos: <span class="font-bold" id="numeroAcertos">0</span></span>
-    <span class="capitalize">tentativas: <span class="font-bold" id="numeroTentativas">0</span></span>
-</div>
-<span id="grupos" class="flex flex-col gap-conatdorGruposAcertados-1"></span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="stroke-zinc-50 size-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
+            </svg>
+            Compartilhe
+        </span>
+    </div>
 
 
-<main id="tabuleiro" class="grid grid-cols-4 gap-2 min-w-6/12 m-auto text-uppercase">
+    <div class="flex my-4 items-center w-6/12 mx-auto justify-between px-3">
+        <div class="w-fit flex items-center gap-x-3">
+            <a href="{{ route('diario')}}" class="inline-block hover:bg-violet-200 rounded-full p-1">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                    <path fill-rule="evenodd"
+                        d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z"
+                        clip-rule="evenodd" />
+                </svg>
+            </a>
+            <span class="font-bold" id="dataAtual"></span>
+            <span class="capitalize">acertos: <span class="font-bold" id="numeroAcertos">0</span></span>
+            <span class="capitalize">tentativas: <span class="font-bold" id="numeroTentativas">0</span></span>
+        </div>
 
-    @for ($i = 0; $i < 16; $i++) 
-        <button type="button"
-            class="bg-violet-100 p-6 rounded-md flex items-center hover:cursor-pointer focus:scale-90 transition duration-300 ease-in-out uppercase hidden"
+        <div class="my-4 w-fit flex items-center gap-x-3">
+            <button class="inline-flex justify-center p-2 hover:bg-violet-200 rounded-lg gap-x-2 self-end">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
+                </svg>
+
+                Dica
+            </button>
+        </div>
+    </div>
+    <span id="grupos" class="flex flex-col gap-x-1 mx-auto w-fit"></span>
+
+    <main id="tabuleiro" class="grid grid-cols-4 gap-2 min-w-6/12 w-6/12 m-auto text-uppercase">
+
+        @for ($i = 0; $i < 16; $i++) <button type="button"
+            class="bg-violet-100 p-5 rounded-md flex items-center hover:cursor-pointer focus:scale-90 transition duration-300 ease-in-out uppercase hidden"
             onClick="verficarJogo(this)">
-        </button>
-    @endfor
+            </button>
+            @endfor
 
-</main>
+    </main>
+
+</div>
 
 </body>
 @endsection
 
 @section('scripts')
 @vite(['resources/js/app.js'])
-@endsection
