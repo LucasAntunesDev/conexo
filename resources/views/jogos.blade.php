311833d4c4c6fd40282ec0fb36334fd78e67d988
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2024-05-03 15:32:43 -0300

    :construction: update: melhorar paginação

--- resources/views/jogos.blade.php
+++ resources/views/jogos.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 @section('content')
 
-<body class=" text-neutral-700">
+<body class=" text-neutral-700 maz-h-screen">
 
     <div class="flex flex-col">
         <div class="flex my-4 items-center w-6/12 mx-auto justify-between px-3">
@@ -21,62 +21,32 @@
             <h2 class="font-bold mx-auto text-2xl">CONEXO</p>
         </div>
 
-        
-
-
-
-        <div class="mx-auto px-4">
-            <div class="grid grid-cols-4 gap-4 bg-violet-900">
-                @foreach ($datas as $data)
-                        <a href="{{ route('diario') }}" target="_blank" class="text-center bg-violet-200 hover:bg-violet-400 text-violet-800 py-5 px-3 rounded-xl w-fit">
-                            {{ $data->data }}
-                        </a>
-                    @if ($loop->iteration % 4 == 0 && !$loop->last)
-                        </div><div class="grid grid-cols-4 gap-4">
-                    @endif
-                @endforeach
-            </div>
-        </div>
-
-
-<!-- Paginação -->
-<div class="mt-4">
-    {{ $datas->links('pagination::tailwind') }}
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
         <main class="flex flex-col gap-2 w-screen justify-center items-center grow">
             <h3 class="mx-auto font-semibold text-xl">Todos os jogos</h3>
 
-            <div class="w-6/12 mx-auto grid grid-cols-4 grid-rows-4 gap-4 p-3">
-                @foreach($jogos as $jogo)
-                <a href="{{ route('diario') }}" target="_blank" class="text-center bg-violet-200 hover:bg-violet-400 text-violet-800 py-5 px-3 rounded-xl w-fit">
-                    @php
-                    $data = \Carbon\Carbon::createFromFormat('Y-m-d', $jogo->data)->format('d/m/Y');
-                    @endphp
-                    {{ $data }}
-
-                </a>
-                @endforeach
-                
-                @foreach($datas as $data => $d)
-{{--                     
-                <pre>
-                    {{var_dump($datas)}}
-                </pre> --}}
-                {{-- <p>{{$data}}</p><br> --}}
-                @endforeach
+            <div class="mx-auto px-4 w-6/12 my-4 grow">
+                <div class="grid grid-cols-4 gap-4 w-auto justify-items-center mb-4">
+                    @foreach ($datas as $data)
+                    <a href="{{ route('diario') }}" target="_blank"
+                        class="text-center bg-violet-200 hover:bg-violet-400 text-violet-800 py-5 px-3 rounded-xl w-fit">
+                        {{ $data->data }}
+                    </a>
+                    @if ($loop->iteration % 4 == 0 && !$loop->last)
+                </div>
+                <div class="grid grid-cols-4 gap-4 w-auto justify-items-center mb-4">
+                    @endif
+                    @endforeach
+                </div>
             </div>
+    
+    
+            <!-- Paginação -->
+            <div class="mt-4">
+                {{-- {{ $datas->links('pagination::tailwind') }} --}}
+                {{ $datas->links('vendor.pagination.tailwind') }}
+            </div>
+
+            
         </main>
     </div>
 
