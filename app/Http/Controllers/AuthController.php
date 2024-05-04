9f4dd6a4deb218a50ad7e8281e21459c593be8da
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2024-05-04 17:34:54 -0300

    :sparkles: métodos de login - AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
-class AuthController extends Controller
-{
-    //
+
+class AuthController extends Controller {
+    public function login() {
+        return view('auth.login');
+    }
+
+    public function fazerLogin(Request $request) {
+        $credentials = $request->only('login', 'senha');
+
+        if (Auth::guard('professor')->attempt($credentials)) {
+            // Autenticação foi bem-sucedida
+            return redirect()->intended('dashboard');
+        }
+
+        // Autenticação falhou
+        return back()->withErrors([
+            'login' => 'As credenciais fornecidas não correspondem aos nossos registros.',
+        ]);
+    }
 }
