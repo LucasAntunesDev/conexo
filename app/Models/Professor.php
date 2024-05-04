e34804679f431fbed5eaa05721cde63b86b2d7ca
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2024-05-04 17:21:27 -0300

    :white_check_mark: add: campos fillable - model de professor

--- app/Models/Professor.php
+++ app/Models/Professor.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 
-class Professor extends Model
+class Professor extends Model implements AuthenticatableContract
 {
+    use Authenticatable;
     use HasFactory;
     protected $table = 'professores';
+    protected $fillable = ['nome', 'login', 'senha'];
+
 }
