724a7217217bf652ba40c21a89a02f3f2d5695ca
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2024-05-04 09:29:23 -0300

    mudei uns negócios no banco...

--- database/migrations/2024_03_02_104551_create_grupos_table.php
+++ database/migrations/2024_03_02_104551_create_grupos_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('nome', 255)->nullable(false);
             $table->unsignedBigInteger('disciplina_id')->unsigned()->nullable(false)->unique(); 
-            $table->foreign('disciplina_id')->references('id')->on('disciplinas')->onUpdate('cascade');
+            // $table->foreign('disciplina_id')->references('id')->on('disciplinas')->onUpdate('cascade');
             $table->timestamps();
         });
     }
