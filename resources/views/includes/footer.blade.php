8e55beb672b479e6b151860e14d2e22a10e5729c
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2024-05-06 07:28:17 -0300

    update: nome do usuário no menu do footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -55,13 +55,13 @@
             id="modalConta">
             <ul class="flex flex-col gap-y-4">
                 <li class="bg-violet-200 rounded-xl *:transition *:duration-300 *:ease-in-out">
-                    <a href="{{ route('professorform', ['id' => auth()->user()->id]) }}" class="inline-flex hover:text-violet-500 py-3 px-6 gap-x-4">
+                    <a href="{{ route('professorform', ['id' => auth()->user()->id]) }}" class="inline-flex hover:text-violet-500 py-3 px-6 gap-x-4 capitalize">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="size-7 transition duration-300 ease-in-out">
                             <path stroke-linecap="round" stroke-linejoin="round"
                                 d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         </svg>
-                        Fulano
+                        {{auth()->user()->nome}}
                     </a>
                 </li>
                 <li class="bg-violet-200 rounded-xl *:transition *:duration-300 *:ease-in-out">
