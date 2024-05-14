730c0b8f5a4a96aa097d870055b34246640a78a0
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2024-05-14 16:50:51 -0300

    fix: favicon em todas as páginas

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('title')</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="icon" href="./favicon.ico">
+    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
 </head>
 
 <body class="text-gray-700 flex flex-col h-screen">
