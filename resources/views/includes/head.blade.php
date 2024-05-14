dbb73d818e378b111db64d46ce37f126cdc2d683
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2024-05-14 16:23:14 -0300

    removida blade de head, com conteúdo dela na blade de layout

--- resources/views/includes/head.blade.php
+++
@@ -1,8 +0,0 @@
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Conexo</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="icon" href="favicon.ico">
-</head>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-@include('includes.head')
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>@yield('title')</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="icon" href="./favicon.ico">
+</head>
 
 <body class="text-gray-700 flex flex-col h-screen">
-    
-        @yield('content')
-        
-        @include('includes.footer')
+
+    @yield('content')
+
+    @include('includes.footer')
     </div>
 </body>
 
