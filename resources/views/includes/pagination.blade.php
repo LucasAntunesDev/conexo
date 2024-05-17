9c0f37c72f658d60e1c99d4796b3251177aca84c
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2024-05-17 19:27:05 -0300

    :lipstick: paginação

--- resources/views/includes/pagination.blade.php
+++ resources/views/includes/pagination.blade.php
@@ -1,6 +1,5 @@
 @if ($paginator->hasPages())
-<nav role="navigation" aria-label="{{ __('Pagination Navigation') }}"
-    class="flex items-center justify-between mx-auto">
+<nav role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="flex items-center justify-between mx-auto">
 
     <div class="flex justify-between flex-1 sm:hidden *:border-none">
         @if ($paginator->onFirstPage())
@@ -36,24 +35,26 @@
                 @if ($paginator->onFirstPage())
                 <span aria-disabled="true" aria-label="{{ __('pagination.previous') }}">
                     <span
-                        class="bg-violet-100 relative inline-flex items-center px-2 py-2 text-sm font-medium text-violet-500 cursor-default rounded-lg leading-5"
+                        class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-violet-100 text-violet-500 h-10 px-4 py-2 gap-1 pr-2.5r"
                         aria-hidden="true">
                         <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd"
                                 d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
                                 clip-rule="evenodd" />
+                            Anterior
                         </svg>
                     </span>
                 </span>
                 @else
                 <a href="{{ $paginator->previousPageUrl() }}" rel="prev"
-                    class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-violet-500 font-bold bg-violet-100 rounded-lg leading-5 hover:text-violet-400 focus:z-10 focus:outline-none focus:ring ring-violet-300 focus:border-violet-300 hover:bg-violet-500 hover:text-zinc-50 focus:bg-violet-500 focus:text-zinc-50 active:bg-violet-100 active:text-violet-500 font-bold transition ease-in-out duration-300 "
+                    class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-violet-100 text-violet-500 h-10 px-4 py-2 gap-1 pl-2.5r"
                     aria-label="{{ __('pagination.previous') }}">
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd"
                             d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
                             clip-rule="evenodd" />
                     </svg>
+                    Anterior
                 </a>
                 @endif
 
@@ -74,12 +75,12 @@
                 @if ($page == $paginator->currentPage())
                 <span aria-current="page">
                     <span
-                        class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-violet-50 font-bold bg-violet-500 cursor-default leading-5 rounded-lg">{{
+                        class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input  hover:bg-violet-100 text-violet-500 h-10 w-10 hover:cursor-pointer border-violet-200">{{
                         $page }}</span>
                 </span>
                 @else
                 <a href="{{ $url }}"
-                    class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium  bg-violet-100 leading-5 hover:text-violet-500 font-bold focus:z-10 focus:outline-none focus:ring ring-violet-300 focus:border-violet-300 hover:bg-violet-500 hover:text-zinc-50 focus:bg-violet-500 focus:text-zinc-50 active:bg-violet-100 active: transition ease-in-out duration-300 rounded-lg"
+                    class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-violet-100 text-violet-500 h-10 w-10"
                     aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
                     {{ $page }}
                 </a>
@@ -91,8 +92,9 @@
                 {{-- Next Page Link --}}
                 @if ($paginator->hasMorePages())
                 <a href="{{ $paginator->nextPageUrl() }}" rel="next"
-                    class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-violet-500 font-bold bg-violet-100 rounded-lg leading-5 hover:text-violet-400 focus:z-10 focus:outline-none focus:ring ring-violet-300 focus:border-violet-300 hover:bg-violet-500 hover:text-zinc-50 focus:bg-violet-500 focus:text-zinc-50 active:bg-violet-100 active:text-violet-500 font-bold transition ease-in-out duration-300 "
+                    class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-violet-100 text-violet-500 h-10 px-4 py-2 gap-1 pl-2.5r"
                     aria-label="{{ __('pagination.next') }}">
+                    Próximo
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd"
                             d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
