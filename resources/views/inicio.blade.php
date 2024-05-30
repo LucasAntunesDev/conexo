27b1249e82c54cb718b11913658c85766535212b
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2024-05-30 08:21:50 -0300

    fix: não mostrar módulos adicionais deslogado

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -51,7 +51,8 @@
                     Jogos
                 </a>
             </div>
-
+            
+            @auth('web')
             <div class="inline-flex items-center p-4 btn-primary">
                 <a href="{{ route('disciplinas') }}" class="inline-flex gap-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
@@ -95,6 +96,7 @@
                     Palavras
                 </a>
             </div>
+            @endif
         </div>
     </div>
 
